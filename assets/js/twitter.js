3a6bd0bf35c5d27015b83ef982c07a67491fe660
Raquel Jarpa <franka.stein@hotmail.com>
2017-11-13 10:52:36 -0300

    No pude poner fecha ni cambiar colores de el contador, pero mi textarea si crece junto a la caja de comentarios

--- assets/js/twitter.js
+++ assets/js/twitter.js
@@ -1,41 +1,48 @@
+function long(){ 
+       var twitter= document.getElementById('comment').value;
+       var contador= document.getElementById("caracteres");
+       var max= 140;
+       contador.innerHTML = max-twitter.length;
+        
+        if(document.getElementById('caracteres').value.length >=120 ){
+        element.style.color = 'green';
+        }if(document.getElementById('caracteres').value.length >=130){
+        element.style.color = 'red';
+    }
+   }{}
 
-/*var comentarios= document.getElementById('comment');
-var boton= document.getElementById('click');
-var span= document.getElementByID('contador');
-var cajadecomentarios= document.getElementByID('newtweet');
+
+
+
+function add(){ /*AÑADIR COMENTARIOS ABAJO*/
+    var tweet= document.getElementById('comment').value;
+    var contenedor= document.getElementById('comentariosabajo');
 
  
+    document.getElementById('comment').value = "";
+    var newtweet= document.createElement('div');
+    newtweet.setAttribute("class", 'cajita')
+    
+   var paragraph= document.createElement('p');
+   paragraph.classList.add('color')
+   var nodoText= document.createTextNode(tweet);
 
-boton.addEventListener("click", function(){/* llamo a el boton + addeventlistener= que haga algo cuando hago click*/
-//creo el input y le doy atributos//
-function long(){
-    	var twitter= document.getElementById('comment').value;
-    	var contador= document.getElementById("caracteres");
-		var max= 140;
-		contador.innerHTML = max-twitter.length;
-    }
-
-
-function add(){
-	var tweet= document.getElementById('comment').value;
-	var contenedor= document.getElementById('comentariosabajo');
-
+   paragraph.appendChild(nodoText);
+   newtweet.appendChild(paragraph);
+   contenedor.appendChild(newtweet);
   
-	document.getElementById('comment').value = "";
-	var newtweet= document.createElement('div');
-	newtweet.setAttribute("class", 'cajita')
-	
-    var paragraph= document.createElement('p');
-    paragraph.classList.add('color')
-    var nodoText= document.createTextNode(tweet);
-
-    paragraph.appendChild(nodoText);
-    newtweet.appendChild(paragraph);
-    contenedor.appendChild(newtweet);
 
 }
 
+var textarea = document.getElementById("comment");
+var limit = 200;
 
-    
+comment.oninput = function() {
+ textarea.style.height = "";
+ textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
+ 
 
+ var Cuadrocomentarios = document.getElementById
+
+}
 
